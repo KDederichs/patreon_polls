41db646c4c779851d8074ef972ddf5b5601101db
Kai Dederichs <kai.dederichs@protonmail.com>
2024-12-31 10:42:35 +0100

    feat: port micro mapper API abstraction

---
+++ api/src/Event/StateProcessor/StatePostPersistEvent.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Event\StateProcessor;
+
+use Symfony\Contracts\EventDispatcher\Event;
+
+class StatePostPersistEvent extends Event
+{
+    public function __construct(
+        private readonly string $entityClass,
+        private readonly mixed $entity,
+    ) {
+    }
+
+    public function getEntityClass(): string
+    {
+        return $this->entityClass;
+    }
+
+    public function getEntity(): mixed
+    {
+        return $this->entity;
+    }
+}
---
+++ api/src/Event/StateProcessor/StatePrePersistEvent.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Event\StateProcessor;
+
+use Symfony\Contracts\EventDispatcher\Event;
+
+class StatePrePersistEvent extends Event
+{
+    public function __construct(
+        private readonly string $entityClass,
+        private readonly mixed $entity,
+        private readonly mixed $dto,
+    ) {
+    }
+
+    public function getEntityClass(): string
+    {
+        return $this->entityClass;
+    }
+
+    public function getEntity(): mixed
+    {
+        return $this->entity;
+    }
+
+    public function getDto(): mixed
+    {
+        return $this->dto;
+    }
+}
